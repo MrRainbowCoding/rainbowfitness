57d65fc868bdd4c648a7d52ada0857f02261523e
MrRainbowCoding <88560300+MrRainbowCoding@users.noreply.github.com>
2023-08-07 17:56:56 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ site/src/components/__tests__/PhotoGallery.spec.js
@@ -0,0 +1,16 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import PhotoGallery from '../PhotoGallery.vue'
+
+describe('PhotoGallery', () => {
+  it('when passed an array of photos it should render properly', () => {
+    const wrapper = mount(PhotoGallery, {
+      props: {
+        photos: ['https://www.example.com/3_4_Sit-Up.jpg']
+      }
+    })
+    // it should render an image with example.jpg as the src
+    expect(wrapper.find('img').attributes('src')).toBe('https://www.example.com/3_4_Sit-Up.jpg')
+  })
+})
